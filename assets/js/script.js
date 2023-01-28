8e54e2b5951ff4d31637fdca44344960cd1f7f88
Lee Vine <leedvine@gmail.com>
2023-01-28 00:20:32 +0000

    initial file set up and input and button click returning latitude and longitude of city to console

---
+++ assets/js/script.js
@@ -0,0 +1,53 @@
+let latitude = "51.5072";
+let longitude = "0.1276";
+let api="2a06c39ab528ae5260e7be3fb9a676eb"
+let queryURL = "https://api.openweathermap.org/data/2.5/forecast?" + "lat=" + latitude + "&lon=" + longitude + "&appid=" + api;
+
+// console.log(queryURL)
+
+// $("#search-button").on("click", function(event) {
+//     event.preventDefault()
+//     // let person = $(this).attr("data-person");
+  
+   
+
+//     $.ajax({
+//       url: queryURL,
+//       method: "GET"
+//     })
+//       .then(function(response) {
+//         var results = response.list;
+//         console.log(results)
+//         console.log(results[1].dt_txt)
+//         console.log(results[9].dt_txt)
+
+//       })
+
+//     })
+
+    // http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=2a06c39ab528ae5260e7be3fb9a676eb how to get latittudes
+
+    
+
+    $("#search-button").on("click", function (event) {
+        let city = $("#search-input").val();
+    console.log(city)
+        event.preventDefault()
+    let queryCity = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=5&appid=2a06c39ab528ae5260e7be3fb9a676eb";
+    console.log(queryCity)
+   
+    $.ajax({
+              url: queryCity,
+              method: "GET"
+            })
+              .then(function(response) {
+                let latitudeResult = response [0].lat;
+                let longitudeResult = response [0].lon;
+                console.log(latitudeResult)
+                console.log(longitudeResult)
+        
+              })
+        
+            })
+
+
