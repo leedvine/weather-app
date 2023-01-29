fff48fef2bfb29889c8e05c41101f7e2d263c841
Lee Vine <leedvine@gmail.com>
2023-01-29 20:56:31 +0000

    Added Divs to index.html via dynamic Javascript for placement of todays weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 // let latitude = "51.5072";
 // let longitude = "0.1276";
-let api="2a06c39ab528ae5260e7be3fb9a676eb"
+let api = "2a06c39ab528ae5260e7be3fb9a676eb";
 // let queryURL = "https://api.openweathermap.org/data/2.5/forecast?" + "lat=" + latitude + "&lon=" + longitude + "&appid=" + api;
 
 // console.log(queryURL)
@@ -8,8 +8,6 @@ let api="2a06c39ab528ae5260e7be3fb9a676eb"
 // $("#search-button").on("click", function(event) {
 //     event.preventDefault()
 //     // let person = $(this).attr("data-person");
-  
-   
 
 //     $.ajax({
 //       url: queryURL,
@@ -25,51 +23,75 @@ let api="2a06c39ab528ae5260e7be3fb9a676eb"
 
 //     })
 
-    // http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=2a06c39ab528ae5260e7be3fb9a676eb how to get latittudes
+// http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=2a06c39ab528ae5260e7be3fb9a676eb how to get latittudes
 
-    
-
-    $("#search-button").on("click", function (event) {
-        let city = $("#search-input").val();
-    console.log(city)
-        event.preventDefault()
-    let queryCity = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=5" + "&appid=" + api;
-    console.log(queryCity)
-   
-    $.ajax({
-              url: queryCity,
-              method: "GET"
-            })
-              .then(function(geoResponse) {
-                let latitudeResult = geoResponse [0].lat;
-                let longitudeResult = geoResponse [0].lon;
-                console.log(latitudeResult)
-                console.log(longitudeResult)
-
-                let queryURL = "https://api.openweathermap.org/data/2.5/forecast?" + "lat=" + latitudeResult + "&lon=" + longitudeResult + "&appid=" + api;
-
-                $.ajax({
-                    url: queryURL,
-                    method: "GET"
-                  })
-              
-                .then(function(response) {
-                            let results = response.list;
-                            console.log(results)
-                            console.log(results[0].dt_txt)
-                            console.log(results[8].dt_txt)
-                    let tempKelvin = results[0].main.temp;
-                    console.log(tempKelvin)
-                    let celcius = Math.round(tempKelvin - 273.15) ;
-                    console.log ("temp converted to celcius = " + celcius)
-
-                  let wind = results[0].wind.speed;
-                  console.log("wind speed = " + wind + " miles per second")
-
-                          })
-
-
+$("#search-button").on("click", function (event) {
+  event.preventDefault();
+  runWeatherSearch()
 })
-            })
+
+function runWeatherSearch () {
+  let city = $("#search-input").val();
+  console.log(city);
+  
+  let queryCity =
+    "http://api.openweathermap.org/geo/1.0/direct?q=" +
+    city +
+    "&limit=5" +
+    "&appid=" +
+    api;
+  console.log(queryCity);
+
+  $.ajax({
+    url: queryCity,
+    method: "GET",
+  }).then(function (geoResponse) {
+    let latitudeResult = geoResponse[0].lat;
+    let longitudeResult = geoResponse[0].lon;
+    console.log(latitudeResult);
+    console.log(longitudeResult);
+
+    let queryURL =
+      "https://api.openweathermap.org/data/2.5/forecast?" +
+      "lat=" +
+      latitudeResult +
+      "&lon=" +
+      longitudeResult +
+      "&appid=" +
+      api;
+
+    $.ajax({
+      url: queryURL,
+      method: "GET",
+    })
+    
+    .then(function (response) {
+      let results = response.list;
+      console.log(results);
+      console.log(results[0].dt_txt);
+      console.log(results[8].dt_txt);
+      let tempKelvin = results[0].main.temp;
+      console.log(tempKelvin);
+      let celcius = Math.round(tempKelvin - 273.15);
+      console.log("temp converted to celcius = " + celcius);
+
+      document.getElementById('todays-temp').innerHTML = celcius;
+
+      let wind = results[0].wind.speed;
+      console.log("wind speed = " + wind + " miles per second");
+
+      let humidity = results[0].main.humidity;
+      console.log("Humidity is = " + humidity + "%");
+    });
+  });
+};
+
+// Create divs
+
+document.getElementById('today').innerHTML += '<div id="todays-weather"></div>';
+document.getElementById('todays-weather').innerHTML += '<h2 id="todays-headline"></h1><div id="todays-temp"></div><div id="todays-wind"></div><div id="todays-humidity"></div>';
+
+
+
 
 
